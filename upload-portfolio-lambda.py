a8b0cfcd62eb8849cc27216e9607526258a7412b
Vinoo Jacob <jacobvinoo@gmail.com>
2018-05-16 11:05:24 +1200

    Updated to include codepipeline integration

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -10,14 +10,28 @@ def lambda_handler(event, context):
 
     s3 = boto3.resource('s3')
 
+    location = {
+        "bucketName": 'portfoliobuild.football.mosyni.com',
+        "objectKey" : 'portfoliobuild.zip'
+
+    }
     try:
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+
+        print "Building portfolio from" + str(location)
+
         portfolio_bucket = s3.Bucket('football.mosyni.com')
-        build_bucket = s3.Bucket('portfoliobuild.football.mosyni.com')
+        build_bucket = s3.Bucket(location["bucketName"])
 
 
         portfolio_zip = StringIO.StringIO()
 
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -27,6 +41,11 @@ def lambda_handler(event, context):
 
         print "Job Done"
         topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully")
+
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+
 
     except:
         topic.publish(Subject="Portfolio Deployment Failed", Message="The Portfolio deployment has failed")
