35083a0bef45f5ff8779f5e38d73df1bf474c242
Vinoo Jacob <jacobvinoo@gmail.com>
2018-05-19 12:11:04 +1200

    Added index2 to test js

---
+++ teamSchedule.js
@@ -0,0 +1,125 @@
+var teamList = [
+  {name: 'Russia', group: 'A'},
+  {name: 'Saudi Arabia', group: 'A'},
+  {name: 'Egypt', group: 'A'},
+  {name: 'Uruguay',group: 'A'},
+  {name: 'Brazil', group: 'E'},
+  {name: 'Switzerland', group: 'E'},
+  {name: 'Costa Rica', group: 'E'},
+  {name: 'Serbia', group: 'E'},
+  {name: 'Portugal', group: 'B'},
+  {name: 'Spain', group: 'B'},
+  {name: 'Morocco',group: 'B'},
+  {name: 'Iran', group: 'B'},
+  {name: 'Germany', group: 'F'},
+  {name: 'Mexico', group: 'F'},
+  {name: 'Sweden', group: 'F'},
+  {name: 'South Korea',group: 'F'},
+  {name: 'France', group: 'C'},
+  {name: 'Australia',group: 'C'},
+  {name: 'Peru', group: 'C'},
+  {name: 'Denmark', group: 'C'},
+  {name: 'Belgium', group: 'G'},
+  {name: 'Panama', group: 'G'},
+  {name: 'Tunisia', group: 'G'},
+  {name: 'England', group: 'G'},
+  {name: 'Argentina', group: 'D'},
+  {name: 'Iceland', group: 'D'},
+  {name: 'Croatia',group: 'D'},
+  {name: 'Nigeria',group: 'D'},
+  {name: 'Poland', group: 'H'},
+  {name: 'Senagal',group: 'H'},
+  {name: 'Columbia',group: 'H'},
+  {name: 'Japan',group: 'H'}
+];
+var matchSchedule = [
+  { 'date': '14-Jun',
+    'matchNo': '1',
+    'team1': 'Russia',
+    'team2': 'Saudi Arabia',
+    'time': '18:00 MSK',
+    'venue': 'Luzhniki Stadium, Moscow',
+    'matchType': 'Group Match'
+  },
+  {
+    'date': '15-Jun',
+    'matchNo': '2',
+    'team1': 'Egypt',
+    'team2': 'Uruguay',
+    'time': '17:00 YEKT',
+    'venue': 'Central Stadium, Yekaterinburg',
+    'matchType': 'Group Match'
+  },
+  {
+    'date': '15-Jun',
+    'matchNo': '3',
+    'team1': 'Morocco',
+    'team2': 'Iran',
+    'time': '18:00 MSK',
+    'venue': 'Krestovsky Stadium, Saint Petersburg',
+    'matchType': 'Group Match'
+  },
+  {
+    'date': '15-Jun',
+    'matchNo': '4',
+    'team1': 'Portugal',
+    'team2': 'Spain',
+    'time': '21:00 MSK',
+    'venue': 'Fisht Olympic Stadium, Sochi',
+    'matchType': 'Group Match'
+  },
+  {
+    'date': '16-Jun',
+    'matchNo': '5',
+    'team1': 'France',
+    'team2': 'Australia',
+    'time': '13:00 MSK',
+    'venue': 'Kazan Arena, Kazan',
+    'matchType': 'Group Match'
+  },
+  {
+    'date': '16-Jun',
+    'matchNo': '6',
+    'team1': 'Peru',
+    'team2': 'Denmark',
+    'time': '19:00 MSK',
+    'venue': 'Mordovia Arena, Saransk',
+    'matchType': 'Group Match'
+  },
+  {
+    'date': '16-Jun',
+    'matchNo': '7',
+    'team1': 'Argentina',
+    'team2': 'Iceland',
+    'time': '16:00 MSK',
+    'venue': 'Otkrytiye Arena, Moscow',
+    'matchType': 'Group Match'
+  },
+  {
+    'date': '16-Jun',
+    'matchNo': '8',
+    'team1': 'Croatia',
+    'team2': 'Nigeria',
+    'time': '21:00 MSK',
+    'venue': 'Kaliningrad Stadium, Kaliningrad',
+    'matchType': 'Group Match'
+  },
+  {
+    'date': '17-Jun',
+    'matchNo': '9',
+    'team1': 'Costa Rica',
+    'team2': 'Serbia',
+    'time': '16:00 SAMT',
+    'venue': 'Cosmos Arena, Samara',
+    'matchType': 'Group Match'
+  },
+  {
+    'date': '17-Jun',
+    'matchNo': '10',
+    'team1': 'Brazil',
+    'team2': 'Serbia',
+    'time': '16:00 SAMT',
+    'venue': 'Cosmos Arena, Samara',
+    'matchType': 'Group Match'
+  }
+];
